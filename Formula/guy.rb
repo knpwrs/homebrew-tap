4d8e2e5097ed859ea2c4b11eb4528b4b26581220
Ken Powers <ken@kenpowers.net>
2022-02-23 20:11:45 -0600

    Bump guy

--- Formula/guy.rb
+++ Formula/guy.rb
@@ -1,8 +1,8 @@
 class Guy < Formula
   desc "Take your terminal to Flavortown"
   homepage "https://github.com/knpwrs/guy"
-  url "https://github.com/knpwrs/guy/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "bd27b97073ebf7d8c2a7a1b62abd5d6b6e87f931400c9a07f2d2e02009ab9f5a"
+  url "https://github.com/knpwrs/guy/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "0206a79e87fff5524365628b620c969f39bd56aeaf1aee4bf8ff8b8fddfc4ad2"
   license "MIT"
   head "https://github.com/knpwrs/guy.git", branch: "main"
 
